53fa144be1a8462b22c88dcb12d65539e586985d
Julian Manzano Osorio <jmanzano@corona.com.co>
2021-09-02 16:48:36 +0000

    Added Vulnerabilidad.cs

---
+++ Corona.RecaudoPagos.BM/Log/Vulnerabilidad.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+
+namespace ZipSlip
+{
+	public class ZipSlipNoncompliant
+	{
+		public void ExtractEntry(IEnumerator<ZipArchiveEntry> entriesEnumerator, string destinationDirectory)
+		{
+			var entry = entriesEnumerator.Current;
+			var destinationPath = Path.Combine(destinationDirectory, entry.FullName);
+			entry.ExtractToFile(destinationPath); // Noncompliant 
+		}
+	}
+}
